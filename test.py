f960d4fced7a1acaffe67f59c0bba621bc0671fc
AlexisMBautista <ing.alexismendezbautista@gmail.com>
2020-11-06 21:20:37 -0600

    comentarios

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-#Para verificar el funcionamiento de selenium.ex
+#Para verificar el funcionamiento de selenium y chromewebdriver
 options = webdriver.ChromeOptions() 
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 driver = webdriver.Chrome(options = options, executable_path = r"C:\CDriver\chromedriver.exe")
